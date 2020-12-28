f006aeaf03fd1b91b5abcc56973a5e6aa6791160
Jack Chen <qiming110038@gmail..com>
2020-12-27 21:26:33 -0800

    updated prefab for sphereBuilder

--- Assets/Scripts/MeshSlicer.cs
+++ Assets/Scripts/MeshSlicer.cs
@@ -34,9 +34,9 @@ public class MeshSlicer : MonoBehaviour
                 {
                     //calculate spawn location
                     float x = 0; float y = 0; float z = 0;
-                    x = (currTriVerts[0].x + currTriVerts[1].x + currTriVerts[2].x)/3;
-                    y = (currTriVerts[0].y + currTriVerts[1].y + currTriVerts[2].y)/3;
-                    z = (currTriVerts[0].z + currTriVerts[1].z + currTriVerts[2].z)/3;
+                    x = (currTriVerts[0].x + currTriVerts[1].x + currTriVerts[2].x)/3+transform.position.x;
+                    y = (currTriVerts[0].y + currTriVerts[1].y + currTriVerts[2].y)/3+transform.position.y;
+                    z = (currTriVerts[0].z + currTriVerts[1].z + currTriVerts[2].z)/3+transform.position.z;
 
                     //create mesh
                     var obj = Instantiate(Triangle, new Vector3(x, y, z), Quaternion.identity);
@@ -45,7 +45,7 @@ public class MeshSlicer : MonoBehaviour
                     Mesh TriMesh = obj.GetComponent<MeshFilter>().mesh;
                     Debug.Log(currTriVerts[0]); Debug.Log(currTriVerts[1]); Debug.Log(currTriVerts[2]);
                     obj.transform.position = Vector3.zero;
-                    TriMesh.vertices = new Vector3[] { currTriVerts[0]/scale, currTriVerts[1]/scale, currTriVerts[2]/scale };
+                    TriMesh.vertices = new Vector3[] { currTriVerts[0]/scale+transform.position, currTriVerts[1]/scale+transform.position, currTriVerts[2]/scale+transform.position};
                     TriMesh.triangles = new int[] { 0, 1, 2 };
                     obj.transform.SetParent(Holder);
                     obj.GetComponent<MeshRenderer>().material.SetColor("_Color", Color.blue);
@@ -65,13 +65,16 @@ public class MeshSlicer : MonoBehaviour
 
         spin = new Vector3(Random.Range(-1, 1), Random.Range(-1, 1), Random.Range(-1, 1));
         target = GameObject.FindGameObjectWithTag("bones").transform;
-        LocateObject(0);
+        //LocateObject(0);
     }
 
+    float velocity = 0;
     // Update is called once per frame
     void Update()
     {
-        Holder.transform.Rotate(Vector3.one * Time.deltaTime*5);
+        LocateObject(0);
+        Holder.transform.Rotate(spin * Time.deltaTime*5);
+        Holder.transform.localScale=Mathf.SmoothDamp(Holder.transform.localScale.x, Expanded ? TargetRadius : 1, ref velocity, 0.35f)*Vector3.one;
         //foreach (Transform t in Holder)
         //{
         //    Debug.Log(t.GetComponent<Renderer>().material.color);
@@ -80,6 +83,10 @@ public class MeshSlicer : MonoBehaviour
         //    bool inside = Vector3.Angle(planeVec, targetVec) < AccAngle;
         //    t.GetComponent<MeshRenderer>().material.SetColor("_Color", inside ? Color.green : Color.blue);
         //}
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Toggle();
+        }
     }
 
     float sphereRadius;
@@ -87,11 +94,19 @@ public class MeshSlicer : MonoBehaviour
     Vector3 VisualSphere;
     void LocateObject(float height)
     {
-        collisionSphere.transform.position = (target.position - transform.position).normalized * TargetRadius;
+        collisionSphere.transform.position = (target.position - transform.position).normalized * TargetRadius+transform.position;
         sphereRadius = 2 * TargetRadius * Mathf.Sin(AccAngle / 2 * Mathf.Deg2Rad);
         collisionSphere.transform.localScale = Vector3.one * sphereRadius;
         Debug.Log(sphereRadius);
         VisualSphere = (target.transform.position - transform.position).normalized * TargetRadius + transform.position;
         //Vector3 center =
+    }
+
+    bool Expanded = false;
+    float radius = 1f;
+    public void Toggle()
+    {
+        Expanded = !Expanded;
+        radius = Expanded ? 10f : 1f;
     }
 }
