4022c129204e6b03eb88021f1b767634914ea539
Khalil <kjaxon6@gmail.com>
2021-05-21 19:02:54 -0700

    images and textbox in game

---
+++ Assets/AutoTyping.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class AutoTyping : MonoBehaviour
+{
+
+    private TMP_Text messageText;
+
+    private void Awake()
+    {
+        messageText = transform.Find("Text (TMP)").GetComponent<TMP_Text>();
+    }
+
+    private void Start()
+    {
+        messageText.text = "Hello World";
+    }
+}
